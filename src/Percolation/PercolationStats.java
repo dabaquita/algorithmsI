a615c121b5e794ac715d2c3f30c66ff487c3275a
Denielle Kirk Abaquita <dabaquita@gmail.com>
2018-06-21 12:12:01 -0400

    Created skeleton of PercolationStats.java

--- src/Percolation/PercolationStats.java
+++ src/Percolation/PercolationStats.java
@@ -5,13 +5,63 @@ import edu.princeton.cs.algs4.StdStats;
 
 /**
  * PercolationStats
- * Purpose:
+ * Purpose: Performs a series of
+ * computational experiments in order
+ * to calculate a percolation threshold value.
  *
  * @author Denielle Kirk Abaquita
- * @version 6/15/18 @ 8:56 AM
+ * @version 6/21/18 @ 12:06 PM
  */
 
 public class PercolationStats {
 
+    // Instance variables
+
+    /** CONSTRUCTOR
+     * @param n - creates n-by-n grid
+     * @param trials - number of independent experiments
+     */
+    public PercolationStats(int n, int trials)
+    {
+
+    }
+
+    /**
+     * @return sample mean of percolation threshold
+     */
+    public double mean()
+    {
+
+    }
+
+    /**
+     * @return sample standard deviation of percolation threshold
+     */
+    public double stddev()
+    {
+
+    }
+
+    /**
+     * @return low endpoint of 95% confidence interval
+     */
+    public double confidenceLo()
+    {
+
+    }
+
+    /**
+     * @return high endpoint of 95% confidence interval
+     */
+    public double confidenceHi()
+    {
+
+    }
+
+    /** Test Client */
+    public static void main(String[] args)
+    {
+
+    }
 
 }
